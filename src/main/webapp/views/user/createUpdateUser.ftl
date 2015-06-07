e33f90d38acfcce9281de14bb8b76a9a508dd259
drmanalo <drmanalo@outlook.com>
2015-06-08 00:43:49 +0100

    Using client-side validation for users

--- src/main/webapp/views/user/createUpdateUser.ftl
+++ src/main/webapp/views/user/createUpdateUser.ftl
@@ -4,28 +4,28 @@
 	<h3><@spring.message "user.create"/></h3>
 	<@spring.bind "form"/>
 	<@spring.bind "roleMap"/>
-	<form method="post" action="saveUser" modelAttribute="form" class="form-horizontal">
+	<form method="post" action="saveUser" modelAttribute="form" class="form-horizontal" id="userform">
 		<@spring.formHiddenInput "form.id"/>
 		<@spring.formHiddenInput "form.action"/>
 		<input type="hidden" name="${_csrf.parameterName}" value="${_csrf.token}"/>
 		<div class="form-group">
     		<label for="username" class="col-sm-2 control-label"><@spring.message "user.email"/></label>
   			<div class="col-sm-4">
-    			<@spring.formInput "form.username", "class='form-control' placeholder='Email'"/>
+    			<@spring.formInput "form.username", "class=\"form-control\" placeholder=\"Email\" autocomplete=\"off\""/>
 				<@spring.showErrors "<br/>", "label label-danger"/>
     		</div>
   		</div>
 		<div class="form-group">
     		<label for="password" class="col-sm-2 control-label"><@spring.message "user.password"/></label>
     		<div class="col-sm-4">
-    			<@spring.formPasswordInput "form.password", "class='form-control' placeholder='Password'"/>
+    			<@spring.formPasswordInput "form.password", "class=\"form-control\" placeholder=\"Password\""/>
 				<@spring.showErrors "<br/>", "label label-danger"/>
     		</div>
   		</div>
   		<div class="form-group">
     		<label for="role" class="col-sm-2 control-label"><@spring.message "user.role"/></label>
 			<div class="col-sm-4">
-				<@spring.formSingleSelect "form.role", roleMap, "class='form-control'"/>
+				<@spring.formSingleSelect "form.role", roleMap, "class=\"form-control\""/>
 			</div>
 		</div>
 		<div class="form-group">
@@ -36,8 +36,40 @@
 		</div>
   		<div class="form-group">
     		<div class="col-sm-offset-2 col-sm-3">
-      			<button type="submit" class="btn btn-default"><@spring.message "user.save"/></button>
+      			<button type="submit" class="btn btn-default" id="button-save"><@spring.message "user.save"/></button>
     		</div>
   		</div>
 	</form>
+	
+	<script type="text/javascript">
+
+		$(function() {
+			
+			$("#userform").validate({
+		        rules: {
+		            "username": {
+		                required: true,
+		                minlength: 6,
+		                email: true
+		            },
+		            "password": {
+		                required: true,
+		                minlength: 6,
+		                maxlength: 40
+		            }
+		        },
+		        messages: {
+		            "username": {
+		                required: "Username is required",
+		                email: "Username must be a valid email address"
+		            },
+		            "password": {
+		                required: "Password is required",
+		                minlength: "Password must be at least 6 characters long"
+		            }
+		        }
+    		});
+			
+		});
+	</script>
 <@footer/>
--- src/test/java/com/manalo/prototype/controller/UserControllerTest.java
+++ src/test/java/com/manalo/prototype/controller/UserControllerTest.java
@@ -7,8 +7,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.ui.Model;
-import org.springframework.validation.Errors;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.testng.annotations.BeforeClass;
@@ -20,139 +18,118 @@ import com.manalo.prototype.form.UserForm;
 import com.manalo.prototype.service.UserService;
 
 public class UserControllerTest {
-	
+
 	private UserService userService;
 	private UserController controller;
-	
-	private Errors errors;
-	private Model model;
 	private RedirectAttributes flash;
-	
+
 	private List<User> users = new ArrayList<>();
-	
+
 	@Test
 	public void login() {
 		String actual = controller.login();
 		assertThat(actual).isEqualTo(ControllerConstant.USER_LOGIN);
 	}
-	
+
 	@Test
 	public void logout() {
 		String actual = controller.logout();
 		assertThat(actual).isEqualTo(ControllerConstant.USER_LOGOUT);
 	}
-	
+
 	@Test
 	public void listUsers() {
-		
+
 		ModelAndView actual = controller.listUsers();
-		
+
 		assertThat(actual.getViewName()).isEqualTo(ControllerConstant.USER_LIST);
 		assertThat(actual.getModelMap().get("users")).isEqualTo(users);
 	}
-	
+
 	@Test
 	public void addUser() {
-		
+
 		ModelAndView actual = controller.addUser();
 		UserForm userForm = (UserForm) actual.getModelMap().get("form");
-		
+
 		assertThat(actual.getViewName()).isEqualTo(ControllerConstant.CREATE_UPDATE_USER);
 		assertThat(userForm.getAction()).isEqualTo(ControllerConstant.CREATE_ACTION);
 	}
-	
+
 	@Test
 	public void editUser() {
-		
+
 		Integer id = 345;
 		UserForm form = new UserForm();
 		when(userService.loadUser(id)).thenReturn(form);
-		
+
 		ModelAndView actual = controller.editUser(id);
 		UserForm userForm = (UserForm) actual.getModelMap().get("form");
-		
+
 		assertThat(actual.getViewName()).isEqualTo(ControllerConstant.CREATE_UPDATE_USER);
 		assertThat(userForm.getAction()).isEqualTo(ControllerConstant.UPDATE_ACTION);
 	}
-	
-	@Test
-	public void saveUser_WithError() {
-		
-		UserForm userForm = new UserForm();
-		when(errors.hasErrors()).thenReturn(true);
-		
-		String actual = controller.saveUser(userForm, errors, model, flash);
-		
-		assertThat(actual).isEqualTo(ControllerConstant.CREATE_UPDATE_USER);
-		verify(model).addAttribute("form", userForm);
-	}
-	
+
 	@Test
 	public void saveUser_CreateAction() {
-		
+
 		String username = "tester";
-		
+
 		UserForm userForm = new UserForm();
 		userForm.setUsername(username);
 		userForm.setAction(ControllerConstant.CREATE_ACTION);
-		
-		when(errors.hasErrors()).thenReturn(false);
-		
-		String actual = controller.saveUser(userForm, errors, model, flash);
-		
+
+		String actual = controller.saveUser(userForm, flash);
+
 		assertThat(actual).isEqualTo("redirect:listUsers");
 		verify(userService).save(userForm);
 		verify(flash).addFlashAttribute("user_added", username);
 	}
-	
+
 	@Test
 	public void saveUser_UpdateAction() {
-		
+
 		String username = "tester";
-		
+
 		UserForm userForm = new UserForm();
 		userForm.setUsername(username);
 		userForm.setAction(ControllerConstant.UPDATE_ACTION);
-		
-		when(errors.hasErrors()).thenReturn(false);
-		
-		String actual = controller.saveUser(userForm, errors, model, flash);
-		
+
+		String actual = controller.saveUser(userForm, flash);
+
 		assertThat(actual).isEqualTo("redirect:listUsers");
 		verify(userService).update(userForm);
 		verify(flash).addFlashAttribute("user_updated", username);
 	}
-	
+
 	@Test
 	public void deleteUser() {
-		
+
 		Integer id = 234;
 		String username = "tester";
 		when(userService.delete(id)).thenReturn(username);
-		
+
 		controller.deleteUser(id, flash);
 		verify(flash).addFlashAttribute("user_deleted", username);
 	}
-	
+
 	@Test
 	public void roleMap() {
 		Map<String, String> actual = controller.roleMap();
 		assertThat(actual.get("ROLE_ADMIN")).isEqualTo("Administrator");
 		assertThat(actual.get("ROLE_USER")).isEqualTo("Web User");
 	}
-	
+
 	@BeforeClass
 	public void setup() {
-		
+
 		userService = mock(UserService.class);
 		when(userService.list()).thenReturn(users);
-		
+
 		controller = new UserController();
 		controller.setUserService(userService);
-		
-		errors = mock(Errors.class);
-		model = mock(Model.class);
+
 		flash = mock(RedirectAttributes.class);
 	}
-	
+
 }
